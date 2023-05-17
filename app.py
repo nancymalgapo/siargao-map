88035e62f233840d863ed7a7477749045abd4fdb
Nancy <nansimalgapo@gmail.com>
2023-05-17 16:49:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ def root():
     return render_template('index.html', markers=markers)
 
 
+@app.route('/landmark/<name>')
+def landmark(name):
+    # Find the landmark with the given name
+    place = next((marker for marker in markers if marker['name'].lower().replace(" ", "-") == name), None)
+
+    if place:
+        return render_template('landmark.html', landmark=place)
+    else:
+        return 'Landmark not found'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
